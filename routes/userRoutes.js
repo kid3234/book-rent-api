44e20586c6de74d56aebead4e88ccb6047d7782d
Kidus Birhane <117932245+kid3234@users.noreply.github.com>
2024-08-10 21:08:05 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,76 @@
 import express from "express";
-import { deleteUser, getAdminOwnerData, getUser, updateUser, updateUserStatus } from "../controllers/userController.js";
+import {
+  approveOwner,
+  deleteUser,
+  getAdminOwnerData,
+  getProfile,
+  getUser,
+  updateUser,
+  updateUserStatus,
+} from "../controllers/userController.js";
 import authenticateToken from "../middlewares/authMidleware.js";
 import { checkAbility } from "../middlewares/abilityMiddleware.js";
-import { getAdminBookData, getAdminDashboardData, getOwnerDashboardData } from "../controllers/bookController.js";
+import {
+  getAdminBookData,
+  getAdminDashboardData,
+  getOwnerDashboardData,
+} from "../controllers/bookController.js";
 
 const userRouter = express.Router();
 
-userRouter.get('/',authenticateToken,checkAbility('read','User'),getUser);
-userRouter.put('/:id',authenticateToken,checkAbility('update','User'),updateUser);
-userRouter.delete('/:id',authenticateToken,checkAbility('delete','User'),deleteUser);
-userRouter.patch('/:id/status',authenticateToken,checkAbility('manage','all'),updateUserStatus);
+userRouter.get("/", authenticateToken, checkAbility("read", "User"), getUser);
 
-userRouter.get("/admin/dashboard",authenticateToken,checkAbility('manage','all'),getAdminDashboardData);
-userRouter.get("/admin/owners",authenticateToken,checkAbility('manage','all'),getAdminOwnerData);
-userRouter.get("/admin/books",authenticateToken,checkAbility('manage','all'),getAdminBookData); 
+userRouter.get("/profile", authenticateToken, getProfile);
+userRouter.put(
+  "/update",
+  authenticateToken,
+  updateUser
+);
+userRouter.delete(
+  "/:id",
+  authenticateToken,
+  checkAbility("delete", "User"),
+  deleteUser
+);
+userRouter.patch(
+  "/:id/status",
+  authenticateToken,
+  checkAbility("manage", "all"),
+  updateUserStatus
+);
+userRouter.patch(
+  "/:id/approve",
+  authenticateToken,
+  checkAbility("manage", "all"),
+  approveOwner
+);
 
-userRouter.get("/owner/dashboard",authenticateToken,checkAbility('read','Book'),getOwnerDashboardData);
+userRouter.get(
+  "/admin/dashboard",
+  authenticateToken,
+  checkAbility("manage", "all"),
+  getAdminDashboardData
+);
+userRouter.get(
+  "/admin/owners",
+  authenticateToken,
+  checkAbility("manage", "all"),
+  getAdminOwnerData
+);
+userRouter.get(
+  "/admin/books",
+  authenticateToken,
+  checkAbility("manage", "all"),
+  getAdminBookData
+);
+
+userRouter.get(
+  "/owner/dashboard",
+  authenticateToken,
+  checkAbility("read", "Book"),
+  getOwnerDashboardData
+);
+
 // userRouter.get("/owner/books",authenticateToken,checkAbility('read','User'),geta)
-
 
 export default userRouter;
