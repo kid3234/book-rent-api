ee13b05075ed7a1b34df94a58a004b783b5b467e
Kidus Birhane <117932245+kid3234@users.noreply.github.com>
2024-08-10 21:11:35 +0300

    Create createAdminUser.js

---
+++ utils/createAdminUser.js
@@ -0,0 +1,32 @@
+import sequelize from "../config/dbConnect.js";
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+export const createAdminUser = async () => {
+  try {
+
+    const adminCount = await User.count({ where: { role: "admin" } });
+
+    if (adminCount === 0) {
+  
+        const hashedPassword = await bcrypt.hash("adminpassword", 10);
+      await User.create({
+        email: "admin@example.com",
+        phone: "0912456789",
+        location: "Admin Location",
+        password: hashedPassword,
+        role: "admin",
+        status: true,
+        approved: true,
+      });
+
+
+
+      console.log("Admin user created successfully.");
+    } else {
+      console.log("Admin user already exists.");
+    }
+  } catch (error) {
+    console.error("Error creating admin user:", error);
+  }
+}
