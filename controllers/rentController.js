84b48d63d4bc3beae230ae42961b87c725298123
kid3234 <birhaneaddis2@gmail.co>
2024-08-12 11:49:53 +0300

    update

--- controllers/rentController.js
+++ controllers/rentController.js
@@ -5,54 +5,7 @@ import Income from "./income.js";
 import sequelize from "../config/dbConnect.js";
 
 
-// export const rentBook = expressAsyncHandler(async (bookId, renterId) => {
-//   const transaction = await sequelize.transaction();
-
-//   try {
-//     const book = await Book.findByPk(bookId, { transaction });
-//     if (!book) throw new Error("Book not found");
-   
-//     const rentPrice = book.price;
-//     const owner = await book.getOwner({ transaction });
-   
-
-//     const rental = await Rental.create(
-//       { bookId, renterId, rentPrice, rentDate: new Date() },
-//       { transaction }
-//     );
-
-//     await book.update(
-//       { availability: "RENTED" },
-//       { where: { id: bookId }, transaction }
-//     );
-
-//     const ownerIncome = book.price * 0.9; 
-//     const systemIncome = book.price * 0.1;
-
-//     await Income.create(
-//       { userId: owner.id, amount: ownerIncome, date: new Date() },
-//       { transaction }
-//     );
-
-//     const adminUser = await User.findOne({ where: { role: "admin" } }); 
-
-//     if (!adminUser) throw new Error("Admin user not found");
-//     await Income.create(
-//       { userId: adminUser.id, amount: systemIncome, date: new Date() }, 
-//       { transaction }
-//     );
-    
-
-//     await transaction.commit();
-//     return rental;
-//   } catch (error) {
-//     await transaction.rollback();
-//     throw error;
-//   }
-// });
-
-
-export const rentBook = expressAsyncHandler(async (bookId, renterId, deviceDate) => {
+export const rentBook = expressAsyncHandler(async (bookId, renterId) => {
   const transaction = await sequelize.transaction();
 
   try {
@@ -61,17 +14,10 @@ export const rentBook = expressAsyncHandler(async (bookId, renterId, deviceDate)
    
     const rentPrice = book.price;
     const owner = await book.getOwner({ transaction });
-    
-  
-    const date = new Date(deviceDate);
-
-    console.log("device date", deviceDate);
-    console.log("converted date",date);
-    
-    
+   
 
     const rental = await Rental.create(
-      { bookId, renterId, rentPrice, rentDate: date },
+      { bookId, renterId, rentPrice, rentDate: new Date() },
       { transaction }
     );
 
@@ -84,7 +30,7 @@ export const rentBook = expressAsyncHandler(async (bookId, renterId, deviceDate)
     const systemIncome = book.price * 0.1;
 
     await Income.create(
-      { userId: owner.id, amount: ownerIncome, date:date },
+      { userId: owner.id, amount: ownerIncome, date: new Date() },
       { transaction }
     );
 
@@ -92,7 +38,7 @@ export const rentBook = expressAsyncHandler(async (bookId, renterId, deviceDate)
 
     if (!adminUser) throw new Error("Admin user not found");
     await Income.create(
-      { userId: adminUser.id, amount: systemIncome, date:date }, 
+      { userId: adminUser.id, amount: systemIncome, date: new Date() }, 
       { transaction }
     );
     
@@ -104,3 +50,4 @@ export const rentBook = expressAsyncHandler(async (bookId, renterId, deviceDate)
     throw error;
   }
 });
+
